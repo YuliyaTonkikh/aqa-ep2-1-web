1ed8d4667a7ce85f5f783aa1bdfda5d1adc05823
YuliyaTonkikh <uliya_i@bk.ru>
2023-03-16 10:44:41 +0700

    add success validation test case

--- src/test/java/ru/netology/CardValidationTest.java
+++ src/test/java/ru/netology/CardValidationTest.java
@@ -3,9 +3,14 @@ package ru.netology;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CardValidationTest {
     private WebDriver driver;
@@ -25,4 +30,19 @@ public class CardValidationTest {
         driver.quit();
         driver = null;
     }
+
+    @Test
+    @DisplayName("Успешная отправка заявки")
+    void shouldSuccessValidation() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79998888888");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("div button")).click();
+
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+
+        assertEquals(expected, actual);
+    }
+
 }
