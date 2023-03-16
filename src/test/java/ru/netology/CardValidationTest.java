2ba9a53b16af1dcdd52689587957cd6554627be2
YuliyaTonkikh <uliya_i@bk.ru>
2023-03-16 10:40:59 +0700

    configure driver setup and teardown

--- src/test/java/ru/netology/CardValidationTest.java
+++ src/test/java/ru/netology/CardValidationTest.java
@@ -1,5 +1,28 @@
 package ru.netology;
 
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class CardValidationTest {
+    private WebDriver driver;
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999/");
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
 }
